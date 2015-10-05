ad4a6e694297f114f03ae4d442c05e26e3d90a59
JumpLink <pascal@jumplink.eu>
2015-10-05 20:29:03 +0200

    added variables example

---
+++ example/variables.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Jade;
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../src/Jade/Jade.php';
+
+$jade = new Jade('/tmp', true);
+$scope = 
+$title = "Hello World";
+$header = "this is append";
+require $jade->cache('index.jade');
